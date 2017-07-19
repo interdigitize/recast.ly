034a40299151ca4f34eee87b16bca6dfaeca14f4
Hack Reactor Student <student@hackreactor.com>
2017-07-19 16:22:48 -0700

    Refactor App to a class component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -22,7 +22,7 @@ class App extends React.Component {
       </div>
     );
   }
-};
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
