b77d73829d150fbc8715a17ac06c8762cf2e813c
Hack Reactor Student <student@hackreactor.com>
2017-07-19 19:34:53 -0700

    Implement searchYouTube GET request

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,21 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  $.ajax({
+    "url": "https://www.googleapis.com/youtube/v3/search",
+    "method": "GET",
+    "data": {
+      "key": options.key,
+      "maxResults": options.max,
+      "q": options.query,
+      "type": "video",
+      "part": "snippet"
+    },
+    success: function (data) {
+      callback(data.items);
+    },
+    error: function () {
+      console.log('Error');
+    }
+  });
 };
 
 window.searchYouTube = searchYouTube;
